637a7288f873f7b6d7fe3a56049b354dec3c95d7
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-06-22 21:56:57 +0530

    hook up checkboxes to state and a bit layout change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [uppercase, setUppercase] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  useEffect(() => {
+    console.log("pass", password);
+    console.log("up", uppercase);
+    console.log("low", lowercase);
+    console.log("num", numbers);
+    console.log("sym", symbols);
+  }, [password, uppercase, lowercase, numbers, symbols]);
+
   return (
     <div className="container">
       <input type="text" name="password" id="password" />
@@ -10,26 +24,53 @@ function App() {
           &#128203;
         </span>
       </button>
-      <button id="generate">Generate</button>
-      <div className="options">
-        <input
-          type="checkbox"
-          id="uppercase"
-          name="uppercase"
-          value="uppercase"
-        />
-        <label for="uppercase"> Include uppercase letters</label>
-        <input
-          type="checkbox"
-          id="lowercase"
-          name="lowercase"
-          value="lowercase"
-        />
-        <label for="lowercase"> Include lowercase letters</label>
-        <input type="checkbox" id="numbers" name="numbers" value="numbers" />
-        <label for="numbers"> Include numbers</label>
-        <input type="checkbox" id="symbols" name="symbols" value="symbols" />
-        <label for="symbols"> Include symbols</label>
+      <button
+        id="generate"
+        onClick={() => setPassword(document.getElementById("password").value)}
+      >
+        Generate
+      </button>
+      <div id="options">
+        <div className="option">
+          <input
+            type="checkbox"
+            id="uppercase"
+            name="uppercase"
+            value={uppercase}
+            onChange={() => setUppercase(!uppercase)}
+          />
+          <label htmlFor="uppercase"> Include uppercase letters</label>
+        </div>
+        <div className="option">
+          <input
+            type="checkbox"
+            id="lowercase"
+            name="lowercase"
+            value={lowercase}
+            onChange={() => setLowercase(!lowercase)}
+          />
+          <label htmlFor="lowercase"> Include lowercase letters</label>
+        </div>
+        <div className="option">
+          <input
+            type="checkbox"
+            id="numbers"
+            name="numbers"
+            value={numbers}
+            onChange={() => setNumbers(!numbers)}
+          />
+          <label htmlFor="numbers"> Include numbers</label>
+        </div>
+        <div className="option">
+          <input
+            type="checkbox"
+            id="symbols"
+            name="symbols"
+            value={symbols}
+            onChange={() => setSymbols(!symbols)}
+          />
+          <label htmlFor="symbols"> Include symbols</label>
+        </div>
       </div>
     </div>
   );
